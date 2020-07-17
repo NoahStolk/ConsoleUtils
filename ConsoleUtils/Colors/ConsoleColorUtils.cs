62047fb51fbb44203b4d08ca6de6fb69008c65b3
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-17 23:57:02 +0200

    Remove NetBase dependency

--- ConsoleUtils/Colors/ConsoleColorUtils.cs
+++ ConsoleUtils/Colors/ConsoleColorUtils.cs
@@ -1,5 +1,4 @@
-﻿using NetBase.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Cmd = System.Console;
@@ -49,22 +48,6 @@ namespace ConsoleUtils.Colors
 			Cmd.SetCursorPosition(0, Cmd.CursorTop);
 			Cmd.Write(new string(' ', Cmd.BufferWidth));
 			Cmd.SetCursorPosition(0, currentLineCursor - offset);
-		}
-
-		public static void WriteBanner(string name, int paddingHor, int paddingVer, char border, ConsoleColor background, ConsoleColor foreground)
-		{
-			Cmd.BackgroundColor = background;
-			Cmd.ForegroundColor = foreground;
-			for (int i = 0; i < paddingVer * 2 + 1; i++)
-			{
-				if (i == 0 || i == paddingVer * 2)
-					Cmd.WriteLine(border.ToString().Repeat(name.Length + paddingHor * 2 + 2));
-				else if (i == paddingVer)
-					Cmd.WriteLine($"{border}{" ".Repeat(paddingHor)}{name}{" ".Repeat(paddingHor)}{border}");
-				else
-					Cmd.WriteLine($"{border}{" ".Repeat(paddingHor * 2 + name.Length)}{border}");
-			}
-			Cmd.ResetColor();
 		}
 	}
 }
